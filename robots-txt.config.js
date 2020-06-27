15c9c934523b1a15be31bb94ae56f12cdf254fc2
Aleksander Fret <alemik@op.pl>
2020-06-27 14:52:15 +0200

    git update robots-txt config

--- robots-txt.config.js
+++ robots-txt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: 'http://www.wilmat.com',
-  sitemap: 'https://www.example.com/sitemap.xml',
+  sitemap: 'http://www.wilmat.com/sitemap.xml',
   env: {
     development: {
       policy: [{ userAgent: '*', disallow: ['/'] }],
